edb2e439384561d0d6d6542d4aa0c23f1e3e882a
Allison Shi <ams9451@nyu.edu>
2024-06-18 09:59:06 -0600

    debug a_func i, j, k, m bounds

--- LCuS.py
+++ LCuS.py
@@ -10,7 +10,6 @@
 # seq[r] == seq[m], or 0 if no such value exists
 def gamma(m, x, seq):
     for r in range(x, 0, -1):
-        # print(f"r={r},m= {m} seq[r-1]={seq[r-1]}, seq[m-1]={seq[m-1]}")
         if seq[r-1] == seq[m-1]:
             return r
     return 0
@@ -18,10 +17,10 @@ def gamma(m, x, seq):
 # assume we have a_{m-1}^{j}(i, k) 
 # for 1 to m-1
 def a_func(i, k, j, m, seq):
-    # print(f"a_func entered. i: {i}, k: {k}, j: {j}, m: {m}")
-    if seq[i-1] == seq[k-1] == seq[m-1]:
-        return h_func(i, k, j, m, seq)
-    if m >= 3:
+    print(f"a_func entered. i: {i}, k: {k}, j: {j}, m: {m}")
+    if 1 <= i and i < j and j <= k and k < m:
+        if seq[i-1] == seq[k-1] == seq[m-1]:
+            return h_func(i, k, j, m, seq)
         return min(a_func(i, k, j, m-1, seq), h_func(i, k, j, m, seq))
     else:
         return 0
@@ -29,17 +28,17 @@ def a_func(i, k, j, m, seq):
 # assume we have a_{m}^{j}(r, s) where \gamma_{m}(i-1) <= r <= i-1 and 
 # \gamma_{m}(k-1) <= s <= k-1
 def h_func(i, k, j, m, seq):
-    # print(f"h_func entered. i: {i}, k: {k}, j: {j}, m: {m}")
-    if gamma(m, i-1, seq) == 0 or gamma(m, k-1, seq) == 0:
-        print(f"gamma k-1: {gamma(m, i-1, seq)}")
-        print(f"gamma i-1: {gamma(m, k-1, seq)}")
+    print(f"h_func entered. i: {i}, k: {k}, j: {j}, m: {m}")
+    x = gamma(m, i-1, seq)
+    y = gamma(m, k-1, seq)
+    if x == 0 or y == 0:
         return m
-    max_a_m_j = 0
-    for r in range(gamma(m, i-1, seq), i):
-        for s in range(gamma(m, k-1, seq), k):
-            if j <= s and r > j:
-                if a_func(r, s, j, m, seq) > max_a_m_j:
-                    max_a_m_j = a_func(r, s, j, m, seq)
-    return max_a_m_j
+    curr_max = 0
+    for r in range(x, i):
+        for s in range(y, k):
+            temp_max = a_func(r, s, j, m, seq)
+            if temp_max > curr_max:
+                curr_max = temp_max
+    return curr_max
 
-print("output:", a_func(3, 6, 4, 10, "abcabcabcd"))
+print("output:", a_func(1, 5, 4, 7, "abcdacad"))
