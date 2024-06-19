6b6e17c3b5781f2ae4eeba2b5decae4769b8326f
robynburger <robynlburger@gmail.com>
2024-06-19 15:59:53 -0600

    add enter

--- LCuS.py
+++ LCuS.py
@@ -3,7 +3,6 @@ import numpy as np
 
 # gamma(m, x, seq) is the largest value of r such that r <= x and 
 # seq[r] == seq[m], or 0 if no such value exists
-
 def gamma(m, x, seq):
     for r in range(x, 0, -1):
         if seq[r-1] == seq[m-1]:
