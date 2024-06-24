e27933475c84524a031fb3959b8607a9af97775b
Allison Shi <ams9451@nyu.edu>
2024-06-24 10:52:23 -0600

    added gamma func

--- LCuS_v3.py
+++ LCuS_v3.py
@@ -1,1 +1,9 @@
 ## File that will hold new algorithm with modified outputs 
+
+# gamma(m, x, seq) is the largest value of r such that r <= x and 
+# seq[r] == seq[m], or 0 if no such value exists
+def gamma(m, x, seq):
+    for r in range(x, 0, -1):
+        if seq[r-1] == seq[m-1]:
+            return r
+    return 0
