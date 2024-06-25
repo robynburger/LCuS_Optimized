2d610f666fd4b612ff9c76f8c96bbe12b8994feb
robynburger <robynlburger@gmail.com>
2024-06-25 10:17:25 -0600

    print case messages

--- LCuS_v3.py
+++ LCuS_v3.py
@@ -24,12 +24,16 @@ def optimized(seq):
             for i in range(1, j):
                 for k in range(j, m):
                     if (i-1 < 1) ^ (k-1 < j):
+                        print("Case 1: (i-1 < 1) ^ (k-1 < j)")
                         h[m, j, i, k] = sys.maxsize 
                     elif (gamma(m, i-1, seq) < 1) ^ (gamma(m, k-1, seq) < j):
+                        print("Case 2: (gamma(m, i-1, seq) < 1) ^ (gamma(m, k-1, seq) < j):")
                         h[m, j, i, k] = sys.maxsize
                     elif ((gamma(m,i-1,seq) < 1) and (gamma(m,k-1,seq) < j)):
+                        print("Case 3: ((gamma(m,i-1,seq) < 1) and (gamma(m,k-1,seq) < j)):")
                         h[m, j, i, k] = m
                     else:
+                        print("Case 4: max")
                         maximum = a[m-1, j, i-1, k-1]
                         if gamma(m, i-1, seq) < i-1:
                             maximum = max(maximum, h[m, j, i-1, k])
@@ -46,18 +50,24 @@ def optimized(seq):
                 for k in range(j, m):
                     if seq[i-1] == seq[k-1] == seq[m-1]:
                         if h[m, j, i, k] == sys.maxsize:
+                            print("Case a: seq[i-1] == seq[k-1] == seq[m-1], h[m, j, i, k] = n/a")
                             a[m, j, i, k] = m
                         else:
+                            print("Case b: seq[i-1] == seq[k-1] == seq[m-1]")
                             a[m, j, i, k] = h[m, j, i, k]
                     elif seq[i-1] == seq[k-1]:
                         if a[m-1, j, i, k] != math.inf and h[m, j, i, k] != math.inf:
+                             print("Case c: a, h defined")
                              a[m, j, i, k] = min(a[m-1, j, i, k], h[m, j, i, k])
                         elif h[m, j, i, k] == math.inf and a[m-1, j, i, k] != math.inf:
+                            print("Case d: a defined, h undef")
                             a[m, j, i, k] = a[m-1, j, i, k]    
                         elif a[m-1, j, i, k] == math.inf and h[m, j, i, k] != math.inf:
+                            print("Case e: h defined, a undef")
                             a[m, j, i, k] = h[m, j, i, k]                       
                     
                     else: 
+                      print("Case f: otherwise")
                       a[m, j, i, k] = 0
     return a           
 
