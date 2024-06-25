b098086d7082c6baf9df2e66af6a4fd2b393839f
Allison Shi <ams9451@nyu.edu>
2024-06-25 09:43:31 -0600

    update LCuS_v3 with adeisha's new changes

--- LCuS_v3.py
+++ LCuS_v3.py
@@ -2,6 +2,7 @@
 # File that will hold new algorithm with modified outputs 
 
 import numpy as np
+import math
 
 # gamma(m, x, seq) is the largest value of r such that r <= x and 
 # seq[r] == seq[m], or 0 if no such value exists
@@ -21,20 +22,21 @@ def optimized(seq):
             # compute h
             for i in range(1, j):
                 for k in range(j, m):
-                    if (i-1 < 1 and k-1 < j) or (gamma(m,i-1,seq) < 1 and gamma(m,k-1,seq) < j):
+                    if (gamma(m,i-1,seq) < 1 and gamma(m,k-1,seq) < j):
                         h[m, j, i, k] = m
+                    elif (gamma(m, i-1, seq) < 1) ^ (gamma(m, k-1, seq) < j):
+                        h[m, j, i, k] = math.inf
+                    elif i-1 < 1 ^ k-1 < j:
+                        h[m, j, i, k] = math.inf
                     else:
-                        if (i-1 < 1) or (k-1 < j) or (gamma(m, i-1, seq) < 1) or (gamma(m, k-1, seq) < j):
-                            h[m, j, i, k] = m
-                        else:
-                            maximum = a[m-1, j, i-1, k-1]
-                            if gamma(m, i-1, seq) < i-1:
-                                maximum = max(maximum, h[m, j, i-1, k])
-                            if gamma(m, k-1, seq) < k-1:
-                                maximum = max(maximum, h[m, j, i, k-1])
-                            h[m, j, i, k] = maximum
-                            #if h[m,j,i,k] > 0:
-                                #print(f"h[{m}, {j}, {i}, {k}] = {h[m, j, i, k]}")
+                        maximum = a[m-1, j, i-1, k-1]
+                        if gamma(m, i-1, seq) < i-1:
+                            maximum = max(maximum, h[m, j, i-1, k])
+                        if gamma(m, k-1, seq) < k-1:
+                            maximum = max(maximum, h[m, j, i, k-1])
+                        h[m, j, i, k] = maximum
+                        #if h[m,j,i,k] > 0:
+                            #print(f"h[{m}, {j}, {i}, {k}] = {h[m, j, i, k]}")
 
             # compute a
             # not checking if a_m-1^j(i, k) is properly defined b/c 
@@ -42,10 +44,15 @@ def optimized(seq):
             for i in range(1, j):
                 for k in range(j, m):
                     if seq[i-1] == seq[k-1] == seq[m-1]:
-                      a[m, j, i, k] = h[m, j, i, k]
+                        if h[m, j, i, k] == math.inf:
+                            a[m, j, i, k] = m
+                        else:
+                            a[m, j, i, k] = h[m, j, i, k]
                     elif seq[i-1] == seq[k-1]:
-                    #   print("si = sk")
-                      a[m, j, i, k] = min(a[m-1, j, i, k], h[m, j, i, k])
+                        if a[m-1, j, i, k] == math.inf and h[m, j, i, k] == math.inf:
+                            a[m, j, i, k] = 0
+                        else:
+                            a[m, j, i, k] = min(a[m-1, j, i, k], h[m, j, i, k])
                     #   print(f"a[{m-1}, {j}, {i}, {k}] = {a[m-1, j, i, k]}")
                     #   print(f"h[{m}, {j}, {i}, {k}] = {h[m, j, i, k]}")
                     else: 
